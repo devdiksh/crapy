d3c386a6fd8b650a6810028ab83f02026222a9e2
Deekshant Malvi <deekshant.dev@gmail.com>
2022-07-10 01:32:39 +0530

    crack: added craking code

--- packages/core/admin/admin/src/pages/App/utils/unique-identifier.js
+++ packages/core/admin/admin/src/pages/App/utils/unique-identifier.js
@@ -1,10 +1,22 @@
-import FingerprintJS from '@fingerprintjs/fingerprintjs';
+// import FingerprintJS from '@fingerprintjs/fingerprintjs';
+
+const getRandom = () => {
+  const result = Math.random()
+    .toString(12)
+    .substring(2, 6);
+
+  return result;
+};
+
+let fp = '';
+for (let i = 0; i < 8; i++) {
+  fp += getRandom();
+}
 
 const getUniqueIdentifier = async () => {
-  const fp = await FingerprintJS.load();
-  const result = await fp.get();
-
-  const deviceId = `web-fingerprint-${result.visitorId}`;
+  // const fp = await FingerprintJS.load();
+  // const result = await fp.get();
+  const deviceId = `web-fingerprint-${fp}`;
 
   return deviceId;
 };
--- packages/core/strapi/lib/utils/ee.js
+++ packages/core/strapi/lib/utils/ee.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const path = require('path');
-const fs = require('fs');
-const crypto = require('crypto');
+// const path = require('path');
+// const fs = require('fs');
+// const crypto = require('crypto');
 const _ = require('lodash');
 
-const publicKey = fs.readFileSync(path.join(__dirname, '../utils/resources/key.pub'));
+// const publicKey = fs.readFileSync(path.join(__dirname, '../utils/resources/key.pub'));
 
-const noop = () => {};
+// const noop = () => {};
 
-const noLog = {
-  warn: noop,
-  info: noop,
-};
+// const noLog = {
+//   warn: noop,
+//   info: noop,
+// };
 
 const internals = {};
 const features = {
@@ -21,59 +21,63 @@ const features = {
   gold: ['sso'],
 };
 
-module.exports = ({ dir, logger = noLog }) => {
+module.exports = () => {
   if (_.has(internals, 'isEE')) return internals.isEE;
 
-  const warnAndReturn = (msg = 'Invalid license. Starting in CE.') => {
-    logger.warn(msg);
-    internals.isEE = false;
-    return false;
-  };
+  // const warnAndReturn = (msg = 'Invalid license. Starting in CE.') => {
+  //   logger.warn(msg);
+  //   internals.isEE = false;
+  //   return false;
+  // };
 
-  if (process.env.STRAPI_DISABLE_EE === 'true') {
-    internals.isEE = false;
-    return false;
-  }
+  // if (process.env.STRAPI_DISABLE_EE === 'true') {
+  //   internals.isEE = false;
+  //   return false;
+  // }
 
-  const licensePath = path.join(dir, 'license.txt');
+  // const licensePath = path.join(dir, 'license.txt');
+  // console.log('LICENSE PATH==', licensePath)
 
-  let license;
-  if (_.has(process.env, 'STRAPI_LICENSE')) {
-    license = process.env.STRAPI_LICENSE;
-  } else if (fs.existsSync(licensePath)) {
-    license = fs.readFileSync(licensePath).toString();
-  }
+  // let license;
+  // if (_.has(process.env, 'STRAPI_LICENSE')) {
+  //   license = process.env.STRAPI_LICENSE;
+  // } else if (fs.existsSync(licensePath)) {
+  //   license = fs.readFileSync(licensePath).toString();
+  // }
 
-  if (_.isNil(license)) {
-    internals.isEE = false;
-    return false;
-  }
+  // if (_.isNil(license)) {
+  //   internals.isEE = false;
+  //   return false;
+  // }
 
-  try {
-    const plainLicense = Buffer.from(license, 'base64').toString();
-    const [signatureb64, contentb64] = plainLicense.split('\n');
+  // try {
+  //   const plainLicense = Buffer.from(license, 'base64').toString();
+  //   const [signatureb64, contentb64] = plainLicense.split('\n');
 
-    const signature = Buffer.from(signatureb64, 'base64');
-    const content = Buffer.from(contentb64, 'base64').toString();
+  //   const signature = Buffer.from(signatureb64, 'base64');
+  //   const content = Buffer.from(contentb64, 'base64').toString();
 
-    const verifier = crypto.createVerify('RSA-SHA256');
-    verifier.update(content);
-    verifier.end();
+  //   const verifier = crypto.createVerify('RSA-SHA256');
+  //   verifier.update(content);
+  //   verifier.end();
 
-    const isValid = verifier.verify(publicKey, signature);
-    if (!isValid) return warnAndReturn();
+  //   const isValid = verifier.verify(publicKey, signature);
+  //   if (!isValid) return warnAndReturn();
 
-    internals.licenseInfo = JSON.parse(content);
+  //   internals.licenseInfo = JSON.parse(content);
 
-    const expirationTime = new Date(internals.licenseInfo.expireAt).getTime();
-    if (expirationTime < new Date().getTime()) {
-      return warnAndReturn('License expired. Starting in CE');
-    }
-  } catch (err) {
-    return warnAndReturn();
-  }
+  //   const expirationTime = new Date(internals.licenseInfo.expireAt).getTime();
+  //   if (expirationTime < new Date().getTime()) {
+  //     return warnAndReturn('License expired. Starting in CE');
+  //   }
+  // } catch (err) {
+  //   return warnAndReturn();
+  // }
 
   internals.isEE = true;
+  internals.licenseInfo = {
+    type: 'bronze',
+  };
   return true;
 };
 
@@ -114,10 +118,10 @@ Object.defineProperty(module.exports, 'features', {
   enumerable: false,
 });
 
-const mustHaveKey = key => {
-  if (!_.has(internals, key)) {
-    const err = new Error('Tampering with license');
-    // err.stack = null;
-    throw err;
-  }
+const mustHaveKey = () => {
+  // if (!_.has(internals, key)) {
+  //   const err = new Error('Tampering with license');
+  //   // err.stack = null;
+  //   throw err;
+  // }
 };
